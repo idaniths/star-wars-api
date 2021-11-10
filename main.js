1491eb9e39c6c30ae872cfc49b4af1e161a2df1b
Daniel Persson <daniel.persson93@hotmail.se>
2021-11-10 20:25:31 +0100

    Fixed bugs in javascript and adjusted inventory

--- main.js
+++ main.js
@@ -3,30 +3,41 @@ function main() {
   let ulDetails = document.querySelector(".details__char");
   const ulInfo = document.querySelector(".details__info");
   let selectedCharacter = null;
-  // Buttons
+
+  /**
+   * Setting upp Buttons
+   */
   const charSpecies = document.querySelector(".species__btn");
   const charHomeworld = document.querySelector(".homeworld__btn");
   const charVehicles = document.querySelector(".vehicles__btn");
   const charStarships = document.querySelector(".starships__btn");
-  //Buttons next "Vehicle" & "Starship"
-  const vehicleInfo = document.getElementById("vehicle__info");
-  const starshipInfo = document.getElementById("starship__info");
+
+  /**
+   * Buttons next vehicle & starship, counters and currentObjekt
+   */
+  const nextVehicle = document.getElementById("vehicle__info");
+  const nextStarship = document.getElementById("starship__info");
   let currentVehicles = [];
   let currentStarship = [];
   let vehicleCounter = 0;
   let starshipCounter = 0;
 
+  /**
+   * Starting function fetching characters
+   */
   function initState() {
     getCharacters("https://swapi.dev/api/people/?page=1");
     showOrHideCharacterActions();
     hideInfoButton();
-
-    // Set up button listeners
     setUpButtonListeners();
   }
-  //Start
+
   initState();
 
+  /**
+   * Setting upp eventListeners
+   * @param buttons - charSpecies, charHomeworld, charVehicles, charStarships, nextVehicles,
+   */
   function setUpButtonListeners() {
     charSpecies.addEventListener("click", () => {
       hideInfoButton();
@@ -37,26 +48,31 @@ function main() {
       getCharacterHomeworld();
     });
     charVehicles.addEventListener("click", () => {
-      starshipInfo.style.visibility = "hidden";
+      nextStarship.style.visibility = "hidden";
       vehicleCounter = 0;
+
       getCharacterVehicles();
     });
     charStarships.addEventListener("click", () => {
-      vehicleInfo.style.visibility = "hidden";
+      nextVehicle.style.visibility = "hidden";
+
       getCharacterStarships();
     });
-    vehicleInfo.addEventListener("click", () => {
+    nextVehicle.addEventListener("click", () => {
       document.getElementById("vehicle__btn").focus();
-      if (currentVehicles.length - 1 == vehicleCounter) {
+
+      /** If the length has reached the end, start from 0 */
+      if (currentVehicles.length - 1 === vehicleCounter) {
         vehicleCounter = 0;
       } else {
         vehicleCounter++;
       }
       setVehicles(currentVehicles, vehicleCounter);
     });
-    starshipInfo.addEventListener("click", () => {
+    nextStarship.addEventListener("click", () => {
       document.getElementById("starship__btn").focus();
-      if (currentStarship.length - 1 == starshipCounter) {
+      /** If the length has reached the end, start from 0 */
+      if (currentStarship.length - 1 === starshipCounter) {
         starshipCounter = 0;
       } else {
         starshipCounter++;
@@ -64,7 +80,11 @@ function main() {
       setStarships(currentStarship, starshipCounter);
     });
   }
-  //<------Fetching Characters-------->
+
+  /**
+   * Fetching Characters
+   * @param selectedCharacter
+   */
   function getCharacters(characters) {
     showLoader(ulChar);
     fetch(characters)
@@ -78,7 +98,10 @@ function main() {
           let names = data.results[i].name;
           const li = document.createElement("li");
           li.appendChild(document.createTextNode(names));
+
+          /** Set up an click listener for all charachters in the list */
           li.addEventListener("click", () => {
+            resetData(); /** Reset data before changing charachter */
             selectedCharacter = data.results[i];
             hideInfoButton();
             removeDetails();
@@ -91,9 +114,17 @@ function main() {
       });
   }
 
-  // SHOW OR HIDE ACTIONS
+  /**
+   * Resets the data when changing characters.
+   */
+  function resetData() {
+    starshipCounter = 0;
+    vehicleCounter = 0;
+  }
 
-  // INFOBUTTONS ------------------------------------
+  /**
+   * Show or hide on infoButtons
+   */
   function showOrHideCharacterActions() {
     showOrHideAction(
       charSpecies,
@@ -136,17 +167,23 @@ function main() {
   }
 
   function hideInfoButton() {
-    vehicleInfo.style.visibility = "hidden";
-    starshipInfo.style.visibility = "hidden";
+    nextVehicle.style.visibility = "hidden";
+    nextStarship.style.visibility = "hidden";
   }
 
-  // HIDE/REMOVE DETAILS ------------------------------------
+  /**
+   * HIDE/REMOVE DETAILS
+   */
   function removeChars() {
     let charList = document.querySelectorAll(".char__list li");
     for (let i = 0; i < charList.length; i++) {
       charList[i].remove();
     }
   }
+
+  /**
+   * Removes the details of the character.
+   */
   function removeDetails() {
     let ulInfo = document.querySelectorAll(
       ".details__info li, .details__info h2"
@@ -161,56 +198,72 @@ function main() {
       ulChar[i].remove();
     }
   }
-  // HIDE/SHOW LOADER ------------------------------------
+
+  /**
+   * @param section - the element where the loader will be placed.
+   */
   function showLoader(section) {
     section.innerHTML = `<div class="loader"></div>`;
   }
+
+  /**
+   * @param section - the element where the loader will be hidden.
+   */
   function hideLoader(section) {
     section.innerHTML = `<div class="loader hidden"></div>`;
   }
 
-  //Function Details
+  /**
+   * Sets the details of the charachter
+   * @param selectedCharacter - the character to be selected
+   */
   function setDetails(selectedCharacter) {
+    let selectedCharacterWeight = selectedCharacter.mass;
+    if (selectedCharacter.mass != "unknown") {
+      selectedCharacterWeight = `${selectedCharacter.mass} kg`;
+    }
+
     ulDetails.innerHTML = `
-  <h2> ${selectedCharacter.name} </h2>
-  <ul>
-        <li>Height: ${selectedCharacter.height}cm </li>
-        <li>Mass:   ${(() => {
-          if (selectedCharacter.mass != "unknown") {
-            return `${selectedCharacter.mass}kg`;
-          } else {
-            return `${selectedCharacter.mass}`;
-          }
-        })()} </li>
+      <h2> ${selectedCharacter.name} </h2>
+      <ul>
+        <li>Height: ${selectedCharacter.height} cm </li>
+        <li>Mass: ${selectedCharacterWeight}</li>
         <li>Hair color: ${selectedCharacter.hair_color} </li>
         <li>Skin color: ${selectedCharacter.skin_color}  </li>
         <li>Eye color: ${selectedCharacter.eye_color} </li>
-        <li> Birth year: ${selectedCharacter.birth_year} </li>
+        <li>Birth year: ${selectedCharacter.birth_year} </li>
         <li>Gender: ${selectedCharacter.gender} </li>
       </ul>
     `;
   }
 
-  // Function Info
+  /**
+   *
+   * @param homeworlds -Prints Characters homeworld
+   */
   function setHomeworlds(homeworlds) {
     ulInfo.innerHTML = `
       <h2> ${homeworlds.name} </h2>
       <ul>
-      <li>Rotation period: ${homeworlds.rotation_period} </li>
-      <li>Orbital period: ${homeworlds.orbital_period} </li>
-      <li>Diameter: ${homeworlds.diameter} </li>
+        <li>Rotation period: ${homeworlds.rotation_period} </li>
+        <li>Orbital period: ${homeworlds.orbital_period} </li>
+        <li>Diameter: ${homeworlds.diameter} </li>
         <li>Climate: ${homeworlds.climate}  </li>
         <li>Gravity: ${homeworlds.gravity} </li>
-        <li> Terrain: ${homeworlds.terrain} </li>
-        </ul>
+        <li>Terrain: ${homeworlds.terrain} </li>
+      </ul>
     `;
   }
-  // Function Species
+
+  /**
+   *
+   * @param species -Prints Characters Specie
+   */
   function setSpecies(species) {
     ulInfo.innerHTML = `
-  <h2> ${species.name} </h2>
+      <h2> ${species.name} </h2>
       <ul>
-      <li>Name: ${species.name} </li>
+        <li>Name: ${species.name} </li>
         <li>Classification: ${species.classification} </li>
         <li>Designation: ${species.designation} </li>
         <li>Average height: ${species.average_height}  </li>
@@ -218,43 +271,53 @@ function main() {
         <li>Hair Colors: ${species.hair_colors} </li>
         <li>Eye Colors: ${species.hair_colors} </li>
         <li>Avarage lifespan: ${species.avarage_lifespan} </li>
-        </ul>
-        `;
+      </ul>
+      `;
   }
-  // Function Vehicles
+
+  /**
+   *
+   * @param vehicle - Prints Characters Vehicles
+   */
   function setVehicles(vehicle) {
     ulInfo.innerHTML = `
-    <h2> ${vehicle[vehicleCounter].name} </h2>
-    <ul>
-    <li>Model: ${vehicle[vehicleCounter].model} </li>
-    <li>Manufacturer: ${vehicle[vehicleCounter].manufacturer} </li>
-    <li>Cost in credits: ${vehicle[vehicleCounter].cost_in_credits}  </li>
-    <li>Length: ${vehicle[vehicleCounter].length} </li>
-    <li>Crew: ${vehicle[vehicleCounter].crew} </li>
-    <li>Passangers: ${vehicle[vehicleCounter].passangers} </li>
-    <li>Cargo capacity: ${vehicle[vehicleCounter].cargo_capacity} </li>
-    <li>Vehicle class: ${vehicle[vehicleCounter].vehicle_class} </li>
-    </ul>
-    `;
-  }
-  // Function Starships
-  function setStarships(starships) {
-    ulInfo.innerHTML = `
-      <h2> ${starships[starshipCounter].name} </h2>
+      <h2> ${vehicle[vehicleCounter].name} </h2>
       <ul>
-      <li>Model: ${starships[starshipCounter].model} </li>
-      <li>Manufacturer: ${starships[starshipCounter].manufacturer} </li>
-      <li>Cost in credits: ${starships[starshipCounter].cost_in_credits}  </li>
-      <li>Length: ${starships[starshipCounter].length} </li>
-      <li>Crew: ${starships[starshipCounter].crew} </li>
-      <li>Passangers: ${starships[starshipCounter].passengers} </li>
-      <li>Cargo capacity: ${starships[starshipCounter].cargo_capacity} </li>
-      <li>Starship class: ${starships[starshipCounter].starship_class} </li>
+        <li>Model: ${vehicle[vehicleCounter].model} </li>
+        <li>Manufacturer: ${vehicle[vehicleCounter].manufacturer} </li>
+        <li>Cost in credits: ${vehicle[vehicleCounter].cost_in_credits}  </li>
+        <li>Length: ${vehicle[vehicleCounter].length} </li>
+        <li>Crew: ${vehicle[vehicleCounter].crew} </li>
+        <li>Passangers: ${vehicle[vehicleCounter].passangers} </li>
+        <li>Cargo capacity: ${vehicle[vehicleCounter].cargo_capacity} </li>
+        <li>Vehicle class: ${vehicle[vehicleCounter].vehicle_class} </li>
       </ul>
     `;
   }
 
-  //<----- FETCHING INFORMATION FOR CHARACTERS ------->
+  /**
+   *
+   * @param  starships -Prints Characters starships
+   */
+  function setStarships(starships) {
+    ulInfo.innerHTML = `
+      <h2> ${starships[starshipCounter].name} </h2>
+      <ul>
+        <li>Model: ${starships[starshipCounter].model} </li>
+        <li>Manufacturer: ${starships[starshipCounter].manufacturer} </li>
+        <li>Cost in credits: ${starships[starshipCounter].cost_in_credits}  </li>
+        <li>Length: ${starships[starshipCounter].length} </li>
+        <li>Crew: ${starships[starshipCounter].crew} </li>
+        <li>Passangers: ${starships[starshipCounter].passengers} </li>
+        <li>Cargo capacity: ${starships[starshipCounter].cargo_capacity} </li>
+        <li>Starship class: ${starships[starshipCounter].starship_class} </li>
+      </ul>
+    `;
+  }
+
+  /**
+   * @returns Fetched Characters  Homeworld
+   */
 
   function getCharacterHomeworld() {
     showLoader(ulInfo);
@@ -267,6 +330,10 @@ function main() {
       });
   }
 
+  /**
+   *
+   * @returns Fetched Characters Specie
+   */
   function getCharacterSpecies() {
     if (selectedCharacter.species.length === 0) {
       return;
@@ -281,6 +348,10 @@ function main() {
       });
   }
 
+  /**
+   *
+   * @returns Fetched Characters Vehicles
+   */
   function getCharacterVehicles() {
     if (!selectedCharacter.vehicles || !selectedCharacter.vehicles[0]) {
       return;
@@ -298,12 +369,16 @@ function main() {
           currentVehicles.push(res);
           setVehicles(currentVehicles);
           if (currentVehicles.length > 1) {
-            vehicleInfo.style.visibility = "visible";
+            nextVehicle.style.visibility = "visible";
           }
         });
     });
   }
 
+  /**
+   *
+   * @returns Fetched Characters Starships
+   */
   function getCharacterStarships() {
     if (!selectedCharacter.starships) {
       return;
@@ -320,13 +395,15 @@ function main() {
           currentStarship.push(res);
           setStarships(currentStarship);
           if (currentStarship.length > 1) {
-            starshipInfo.style.visibility = "visible";
+            nextStarship.style.visibility = "visible";
           }
         });
     });
   }
 
-  //<-------COUNTER FOR CHARACTERS-------->
+  /**
+   * @param {next previous} -Counter for next page
+   */
   generalCounter = () => {
     let counter = 1;
     let counterOne = document.querySelector(".counterOne");
